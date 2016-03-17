bc618252d335dcd384d693ea45ff651c159ddf1e
codeitloadit <brian@codeitload.it>
2016-03-16 22:49:51 -0700

    Refactoring.

--- redis-mux/main.go
+++ redis-mux/main.go
@@ -12,7 +12,6 @@ import (
 
 var port string
 var hosts []string
-var clientData ClientData
 
 type ClientData struct {
 	readers []io.Reader
@@ -38,6 +37,9 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		defer hostConn.Close()
+
+		var clientData ClientData
 
 		for _, host := range hosts {
 			conn, err := net.Dial("tcp", host)
@@ -48,23 +50,23 @@ func main() {
 			clientData.writers = append(clientData.writers, conn)
 		}
 
-		go func() {
-			for {
-				mr := io.MultiReader(clientData.readers...)
-				_, err := io.Copy(hostConn, mr)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-		}()
+		go sendFromHostToClients(hostConn, clientData)
+		go sendFromClientsToHost(hostConn, clientData)
+	}
+}
 
-		go func() {
-			mw := io.MultiWriter(clientData.writers...)
-			_, err := io.Copy(mw, hostConn)
-			if err != nil {
-				log.Fatal(err)
-			}
-			hostConn.Close()
-		}()
+func sendFromHostToClients(hostConn net.Conn, clientData ClientData) {
+	mr := io.MultiReader(clientData.readers...)
+	_, err := io.Copy(hostConn, mr)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func sendFromClientsToHost(hostConn net.Conn, clientData ClientData) {
+	mw := io.MultiWriter(clientData.writers...)
+	_, err := io.Copy(mw, hostConn)
+	if err != nil {
+		log.Fatal(err)
 	}
 }

1cdf264af131bcde2c76a1e0a27ff0b7c50b9817
codeitloadit <brian@codeitload.it>
2016-03-18 00:01:45 -0700

    Fan in and fan out working for single (first) host connection.

--- redis-mux/main.go
+++ redis-mux/main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"os"
@@ -13,10 +11,7 @@ import (
 var port string
 var hosts []string
 
-type ClientData struct {
-	readers []io.Reader
-	writers []io.Writer
-}
+var conns []net.Conn
 
 func init() {
 	port = os.Getenv("REDIS_MUX_PORT")
@@ -43,34 +38,81 @@ func main() {
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	var clientData ClientData
-
+func handleConnection(hostConn net.Conn) {
 	for _, host := range hosts {
 		conn, err := net.Dial("tcp", host)
 		if err != nil {
 			log.Fatal(err)
 		}
-		clientData.readers = append(clientData.readers, bufio.NewReader(conn))
-		clientData.writers = append(clientData.writers, conn)
+
+		conns = append(conns, conn)
 	}
 
-	go sendFromHostToClients(conn, clientData)
-	go sendFromClientsToHost(conn, clientData)
+	go sendFromHostToClients(hostConn)
 }
 
-func sendFromHostToClients(hostConn net.Conn, clientData ClientData) {
-	mw := io.MultiWriter(clientData.writers...)
-	_, err := io.Copy(mw, hostConn)
-	if err != nil {
-		log.Fatal(err)
+func sendFromHostToClients(hostConn net.Conn) {
+	ch := make(chan []byte)
+	eCh := make(chan error)
+
+	go func() {
+		for {
+			data := make([]byte, 1024)
+			n, err := hostConn.Read(data)
+			data = data[:n]
+			if err != nil {
+				eCh <- err
+				return
+			}
+			ch <- data
+		}
+	}()
+
+	for {
+		select {
+		case data := <-ch:
+			fmt.Print("Sending:\t")
+			fmt.Printf("%q\n", string(data))
+			for _, conn := range conns {
+				conn.Write(data)
+				go sendFromClientsToHost(hostConn)
+			}
+		case err := <-eCh:
+			fmt.Println(err)
+			break
+		}
 	}
 }
 
-func sendFromClientsToHost(hostConn net.Conn, clientData ClientData) {
-	mr := io.MultiReader(clientData.readers...)
-	_, err := io.Copy(hostConn, mr)
-	if err != nil {
-		log.Fatal(err)
+func sendFromClientsToHost(hostConn net.Conn) {
+	for _, conn := range conns {
+		ch := make(chan []byte)
+		eCh := make(chan error)
+
+		go func() {
+			for {
+				data := make([]byte, 1024)
+				n, err := conn.Read(data)
+				data = data[:n]
+				if err != nil {
+					eCh <- err
+					return
+				}
+				ch <- data
+			}
+		}()
+
+		for {
+			select {
+			case data := <-ch:
+				fmt.Print("Received:\t")
+				fmt.Printf("%q\n", string(data))
+				hostConn.Write(data)
+			case err := <-eCh:
+				fmt.Println(err)
+				break
+			}
+		}
 	}
+
 }
